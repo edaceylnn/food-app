13877f4b919773f55767344b05c2ba85d49a2891
Eda Ceylan <62337318+edaceylnn@users.noreply.github.com>
2023-06-13 13:18:32 +0300

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                 <div onClick={() => setNav(!nav)} className='cursor-pointer'>
                     <AiOutlineMenu size={30} />
                 </div>
-                <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Best <span className='font-bold'>Easts</span></h1>
+                <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Best <span className='font-bold'>Foods</span></h1>
                 <div className='hidden lg:flex items-center bg-gray-200 rounded-full text-[14px]'>
                     <p className='bg-black text-white rounded-full p-2'>Delivery</p>
                     <p className='p-2'>Pickup</p>
